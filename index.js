321c6c0dfa6864d063e5f62fd16eaf199926356d
Jack Robbins <jackrobbins1@gmail.com>
2019-03-04 21:19:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(arr, name){
   arr.push(name);
-  return `Welcome, ${name}. You are number ${arr.length + 1} in line.`
+  return `Welcome, ${name}. You are number ${arr.length} in line.`
 }
 
 function nowServing(arr) {
