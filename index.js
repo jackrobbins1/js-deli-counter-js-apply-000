f7abc2d6a1ed8d91695db1d11026d7301a033d54
Jack Robbins <jackrobbins1@gmail.com>
2019-03-04 21:00:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function takeANumber(arr, name){
   arr.pop(name);
   return `Welcome, ${name}. You are number ${arr.length + 1} in line.`
 }
+
+function nowServing(arr) {
+  arr.shift();
+
+  if (arr === []) {
+    return `There is nobody waiting to be served!`;
+  }
+}
